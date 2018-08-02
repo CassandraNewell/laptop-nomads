a93923c7e776a866c243ac9e590a9fe9d3a493fe
Cassandra Newell <cassandra.newell@protonmail.com>
2018-08-01 21:44:22 -0400

    some styling

--- app/javascript/components/ReviewTile.js
+++ app/javascript/components/ReviewTile.js
@@ -1,13 +1,38 @@
-import React from 'react';
+import React, { Component } from 'react';
 
-const ReviewTile = (props) => {
-  return(
-    <div className="review">
-      <h5>{props.fullname} gave a rating of {props.rating}/5</h5>
-      <p>{props.body}</p>
-      <span className="fa fa-star"></span>
-    </div>
-  )
+class ReviewTile extends Component {
+  constructor(props){
+    super(props)
+    this.state = {
+    }
+  }
+
+  render(){
+    let numbers = [1, 2, 3, 4, 5]
+    let stars = numbers.map((number) => {
+      if (number > this.props.rating) {
+        return(
+          <i className="far fa-star"></i>
+        )
+      } else {
+        return(
+          <i className="fas fa-star"></i>
+        )
+      }
+    })
+
+    return(
+      <div className="review grid-x">
+        <div className="cell small-8">
+          <h4>{this.props.fullname} says</h4>
+          <p>{this.props.body}</p>
+        </div>
+        <div className="cell small-4">
+          {stars}
+        </div>
+      </div>
+    )
+  }
 }
 
 
