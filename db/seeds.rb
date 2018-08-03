11e8df2d164c32c15e6df949c6cca4c916f0df00
Cassandra Newell <cassandra.newell@protonmail.com>
2018-08-03 10:27:39 -0400

    improved seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,8 +5,8 @@ Venue.create(
   name: "Forge Baking Co",
   address: "Somerville Ave",
   description: "Forge Baking Co, sister cafe of Diesel and Bloc, sells various café drinks as well as pastries and sandwiches. They have free wifi, but the wifi can get slow during peak hours (10 AM - 4 PM). Single-person and group tables available; some outdoor, table-less seating available (except in winter). Has small ice cream shoppe adjacent.",
-  open_time: "7 AM, daily",
-  close_time: "8 PM, daily",
+  open_time: "7 AM daily",
+  close_time: "8 PM daily",
   venue_url: "http://www.forgebakingco.com",
   photo_url: Rack::Test::UploadedFile.new(Rails.root.join('app/assets/images/venue/forge_baking_co.jpg'), 'image/jpeg')
 )
@@ -14,15 +14,22 @@ Venue.create(
 Venue.create(
   name: "Three Little Figs",
   address: "Highland Ave",
+  description: "Cute little cafe on Highland Ave, near the firestation. Has outdoor seating. Weekdays: wifi, computers OK. Weekends: no wifi, no computers. Delicious pasteries and breakfast food. Home to two of three of the 2018 Somerville Best Barista contenders.",
+  open_time: "7 AM M-F, 8 AM Sa-Su",
+  close_time: "4 PM",
   photo_url: Rack::Test::UploadedFile.new(Rails.root.join('app/assets/images/venue/three_little_figs.jpg'), 'image/jpeg')
 )
 
 Venue.create(
   name: "Blue Shirt Cafe",
   address: "Davis Square",
+  description: "<b>Venue closing on July 29, 2018!</b>Free wifi, great wraps/bowls (fairly large portions, ~$7-8). Small drink selection. Usually lots of seating available; be warned that the management will kick you out if you don't buy something. Little bit of outdoor seating.",
+  open_time: "8 AM daily",
+  close_time: "10 PM daily",
   photo_url: Rack::Test::UploadedFile.new(Rails.root.join('app/assets/images/venue/blue_shirt_cafe.jpg'), 'image/jpeg')
 )
 
+# Create users
 User.create!(
   first_name: "Cassandra",
   last_name: "Newell",
@@ -111,6 +118,7 @@ Review.create!(
   user: User.second
 )
 
+# Create votes
 ## Same user has each possible vote on three reviews for same venue
 ReviewVote.create!(
   vote: -1,
